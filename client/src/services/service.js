e54c0ce09d79dbf3a8e5c64fb267608dda40b4a0
hermann.diakite <hermann.diakite@epita.fr>
2018-06-19 11:20:42 +0200

    fix datasource, add url api

--- client/src/services/service.js
+++ client/src/services/service.js
@@ -2,9 +2,12 @@ import Api from './Api'
 
 export default {
   fetchPosts () {
-    return Api().get('posts')
+    return Api().get('posts');
   },
   getTests (){
-      return Api().get('tests')
+      return Api().get('tests');
+  },
+  getTheme () {
+    return Api().get('themes');
   }
 }
