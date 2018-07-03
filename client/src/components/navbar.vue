8823fd134cd1b68b7735a61b5c3f015531166661
hermann.diakite <hermann.diakite@epita.fr>
2018-07-03 13:28:56 +0200

    master of commit

--- client/src/components/navbar.vue
+++ client/src/components/navbar.vue
@@ -1,13 +1,12 @@
 <template>
   <b-navbar toggleable="md" type="light">
-    <b-navbar-brand href="#">Theming</b-navbar-brand>
+    <b-navbar-brand href="#"><router-link class="nav-link" to="Home">Theming</router-link></b-navbar-brand>
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav>
-        <b-nav-item><router-link class="nav-link" to="Home">Home</router-link></b-nav-item>
         <b-nav-item><router-link class="nav-link" to="Testing">Tester</router-link></b-nav-item>
-        <b-nav-item><router-link class="nav-link" to="Train">Entrainement</router-link></b-nav-item>
-        <b-nav-item><router-link class="nav-link" to="Docs">Documentations</router-link></b-nav-item>
+        <b-nav-item><router-link class="nav-link" to="Train">Entrainer</router-link></b-nav-item>
+        <b-nav-item><router-link class="nav-link" to="Manage">Gerer</router-link></b-nav-item>
         <b-nav-item><router-link class="nav-link" to="About">A propos</router-link></b-nav-item>
       </b-navbar-nav>
     </b-collapse>
